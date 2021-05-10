9db150f7731461e23a3b3ac27c1b04e3d56b0100
thembaxaba <69608635+thembaxaba@users.noreply.github.com>
2021-05-10 01:20:57 -0700

    Add files via upload

--- lib/screens/checkout.dart
+++ lib/screens/checkout.dart
@@ -4,11 +4,18 @@ import 'package:aishop/components/checkoutdelivary.dart';
 import 'package:aishop/components/checkoutpayment.dart';
 import 'package:flutter/material.dart';
 import 'package:aishop/components/checkoutaddress.dart';
-import 'package:aishop/components/checkoutorderreview.dart';
-
+import 'package:aishop/components/ORDERREVIEW.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import '../components/google_round_button.dart';
 import '../components/round_button.dart';
 import '../components/round_button.dart';
+
+User _user = FirebaseAuth.instance.currentUser!;
+
+final CollectionReference usersRef = FirebaseFirestore.instance.collection('Users')
+    .doc(_user.uid).collection("Cart");
+var products_on_cart = [];
 
 class CheckOutPage extends StatefulWidget {
   @override
@@ -16,6 +23,32 @@ class CheckOutPage extends StatefulWidget {
 }
 
 class _CheckOutPageState extends State<CheckOutPage> {
+
+  Getdata() async{
+    return await usersRef;
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    getUsers();
+
+    super.initState();
+  }
+
+  getUsers(){
+    usersRef.get().then((QuerySnapshot snapshot){
+      for(int i = 0; i < snapshot.docs.length; ++i)
+      {
+        print('name: $snapshot');
+        products_on_cart.add(snapshot.docs[i].data());
+        print(snapshot.docs[i].data());
+      }
+
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
