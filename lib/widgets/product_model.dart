4636d463f0a6ae3783b86c6e369ba7c8daf46682
Hardi-hood <2130564@students.wits.ac.za>
2021-05-18 18:29:22 +0200

    click tracker

--- lib/widgets/product_model.dart
+++ lib/widgets/product_model.dart
@@ -1,3 +1,4 @@
+import 'package:aishop/Services/historytracker.dart';
 import 'package:aishop/components/databasemanager.dart';
 import 'package:aishop/icons/icons.dart';
 import 'package:aishop/utils/cart.dart';
@@ -18,131 +19,19 @@ class ProductCard extends StatefulWidget {
   final String price;
 
   ProductCard(
-    this.id,
-    this.imgUrl,
-    this.name,
-    this.description,
-    this.price,
-  );
+      this.id,
+      this.imgUrl,
+      this.name,
+      this.description,
+      this.price,
+      );
 
   @override
   State<StatefulWidget> createState() {
     return _ProductCard();
   }
 }
-/*
-class _ProductCard extends State<ProductCard> {
 
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
-      child: Container(
-          width: 310,
-          height: 350,
-          decoration: BoxDecoration(
-              color: white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.blueGrey,
-                  blurRadius: 5,
-                )
-              ],
-              borderRadius: BorderRadius.circular(20)),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                InkWell(
-                    onTap: () {
-                      //on tap modal pop up
-                      Modal(context, widget.imgUrl, widget.name, widget.description, widget.price);
-                      DataService().increment(widget.name);
-                    },
-                    splashColor: Colors.white30,
-                    customBorder: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Container(
-                            child: Padding(
-                          padding: EdgeInsets.all(0.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              SizedBox(
-                                height: 10,
-                              ),
-                              //image from db
-                              Image.network(
-                                widget.imgUrl,
-                                width: 180,
-                                height: 280,
-                                fit: BoxFit.fitWidth,
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              //text
-                              Text(
-                                "Name: " + widget.name,
-                                style: TextStyle(
-                                    fontSize: 15, fontWeight: FontWeight.bold),
-                              ),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              //price
-                              Text("Price: R " + widget.price,
-                                  style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold)),
-                            ],
-                          ),
-                        )))),
-                Center(
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding:
-                            const EdgeInsets.fromLTRB(30.0, 20.0, 20.0, 0.0),
-                        child: IconButton(
-                          icon: Icon(
-                            AIicons.wishlist,
-                            color: lightblack,
-                            size: 35,
-                          ),
-                          onPressed: () {},
-                        ),
-                      ),
-                      Padding(
-                          padding:
-                              const EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 0.0),
-                          child: ElevatedButton.icon(
-                            onPressed: () {},
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.black54,
-                            ),
-                            icon: Icon(
-                              Icons.add_shopping_cart_rounded,
-                              size: 20,
-                              color: white,
-                            ),
-                            label: Text(
-                              "ADD TO CART",
-                              style: TextStyle(
-                                  fontSize: 15.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: white),
-                            ),
-                          )),
-                    ],
-                  ),
-                )
-              ])),
-    );
-  }
-}
-*/
 //model for one item
 //take product details as parameters and return and clickable countainer that displays the image & price of product
 
@@ -167,10 +56,14 @@ class _ProductCard extends State<ProductCard> {
               ],
               borderRadius: BorderRadius.circular(20)),
           child:
-              Column(crossAxisAlignment: CrossAxisAlignment.center, children: <
-                  Widget>[
+          Column(crossAxisAlignment: CrossAxisAlignment.center, children: <
+              Widget>[
             InkWell(
                 onTap: () {
+                  //add to history clicks
+                  HistoryTracker.addToHistory(
+                      widget.id, widget.imgUrl, widget.description, widget.name, widget.price
+                  );
                   //on tap modal pop up
                   Modal(context, widget.imgUrl, widget.name, widget.description,
                       widget.price);
@@ -183,43 +76,43 @@ class _ProductCard extends State<ProductCard> {
                     padding: const EdgeInsets.all(4.0),
                     child: Container(
                         child: Padding(
-                      padding: EdgeInsets.all(5.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: <Widget>[
-                          //image from db
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Image.network(
-                              widget.imgUrl,
-                              width: 180,
-                              height: 200,
-                              fit: BoxFit.fitWidth,
-                            ),
+                          padding: EdgeInsets.all(5.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              //image from db
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Image.network(
+                                  widget.imgUrl,
+                                  width: 180,
+                                  height: 200,
+                                  fit: BoxFit.fitWidth,
+                                ),
+                              ),
+                              //text
+                              Padding(
+                                padding: const EdgeInsets.all(4.0),
+                                child: Text(
+                                  widget.name,
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: "Nunito Sans"),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              //price
+                              Text("R " + widget.price,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w900,
+                                    color: Colors.orange,
+                                  )),
+                            ],
                           ),
-                          //text
-                          Padding(
-                            padding: const EdgeInsets.all(4.0),
-                            child: Text(
-                              widget.name,
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.bold,
-                                  fontFamily: "Nunito Sans"),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          //price
-                          Text("R " + widget.price,
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w900,
-                                color: Colors.orange,
-                              )),
-                        ],
-                      ),
-                    )))),
+                        )))),
             Center(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -269,27 +162,27 @@ class _ProductCard extends State<ProductCard> {
                               primary: Colors.black54, elevation: 0.1),
                           icon: add
                               ? Icon(
-                                  Icons.done_all_sharp,
-                                  size: 20,
-                                  color: Colors.green,
-                                )
+                            Icons.done_all_sharp,
+                            size: 20,
+                            color: Colors.green,
+                          )
                               : Icon(Icons.add_shopping_cart_rounded,
-                                  size: 20, color: white),
+                              size: 20, color: white),
                           label: add
                               ? Text(
-                                  "ADDED",
-                                  style: TextStyle(
-                                      fontSize: 15.0,
-                                      fontWeight: FontWeight.bold,
-                                      color: white),
-                                )
+                            "ADDED",
+                            style: TextStyle(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold,
+                                color: white),
+                          )
                               : Text(
-                                  "ADD TO CART",
-                                  style: TextStyle(
-                                      fontSize: 15.0,
-                                      fontWeight: FontWeight.bold,
-                                      color: white),
-                                ))),
+                            "ADD TO CART",
+                            style: TextStyle(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold,
+                                color: white),
+                          ))),
                 ],
               ),
             )
