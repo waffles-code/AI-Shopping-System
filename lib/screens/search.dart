ddcb9156044fb970f2a1bf9d7f819684b73d6936
RefentseLedwaba <81798520+RefentseLedwaba@users.noreply.github.com>
2021-05-26 07:27:12 +0200

    Add files via upload

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,16 +1,17 @@
 import 'dart:ui';
 import 'package:aishop/components/searchservice.dart';
+import 'package:aishop/widgets/product_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Search extends StatefulWidget {
+
   @override
   SearchState createState() => SearchState();
 }
 
 class SearchState extends State<Search> {
 
-  bool isSearching = false;
   var queryResultSet = [];
   var tempSearchStore = [];
 
@@ -24,10 +25,10 @@ class SearchState extends State<Search> {
     var capitalizedValue =
         value.substring(0, 1).toUpperCase() + value.substring(1);
 
-    if (queryResultSet.length == 0 && value.length == 1) {
+    if (queryResultSet.length == 0 && value.length > 0) {
       SearchService().searchByName(capitalizedValue).then((QuerySnapshot mydocs) {
         for (int i = 0; i < mydocs.docs.length; ++i) {
-          queryResultSet.add(mydocs.docs[i].data());
+          queryResultSet.add(mydocs.docs[i]);
           setState(() {
             tempSearchStore.add(queryResultSet[i]);
           });
@@ -86,22 +87,23 @@ class SearchState extends State<Search> {
           ),
           // iconTheme: IconThemeData(color: Colors.white)),
       //Body of the home page
-      body:
+      body: tempSearchStore.isNotEmpty ?
         ListView(children:
           <Widget>[
-            SizedBox(height: 10.0),
+            SizedBox(height: 15.0, width: 10.0),
             GridView.count(
-                padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                crossAxisCount: 4,
+                padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 10),
+                crossAxisCount: 5,
+                childAspectRatio: (200 / 300),
                 crossAxisSpacing: 4.0,
                 mainAxisSpacing: 4.0,
                 primary: false,
                 shrinkWrap: true,
                 children: tempSearchStore.map((element) {
-                  return SearchService().buildResultCard(context, element);
+                  return ProductCard(element.id.toString(), element.data()['url'].toString(), element.data()['name'].toString(), element.data()['description'].toString(), element.data()['price'].toString());
                 }).toList())
           ]
-        )
+        ): new Text("")
     );
   }
 }
