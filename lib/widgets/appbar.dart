22470356f2d1f03550338c58da3ec15fa4a85393
Mmasehume <2089198@students.wits.ac.za>
2021-06-06 01:38:07 +0200

    added navigation to appbar actions

--- lib/widgets/appbar.dart
+++ lib/widgets/appbar.dart
@@ -1,8 +1,12 @@
+import 'package:aishop/Services/prod_num_badges.dart';
 import 'package:aishop/icons/icons.dart';
+import 'package:aishop/screens/checkout.dart';
+import 'package:aishop/screens/wishlistscreen.dart';
 import 'package:aishop/utils/authentication.dart';
 import 'package:badges/badges.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+// import 'package:/flutter/widgets.dart';
 
 import '../theme.dart';
 
@@ -21,7 +25,7 @@ class constants {
 }
 
 class MyAppBar extends AppBar {
-  MyAppBar({Key? key, Widget? title})
+  MyAppBar({Key? key, Widget? title, required BuildContext context})
       : super(
             key: key,
             title: title,
@@ -40,8 +44,8 @@ class MyAppBar extends AppBar {
                 ),
                 onPressed: () {
                   // Get.to(W());
-                  // Navigator.of(context).push(MaterialPageRoute(
-                  //     builder: (BuildContext context) => W()));
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context) => WishlistPage()));
                 },
               ),
               Padding(
@@ -58,11 +62,10 @@ class MyAppBar extends AppBar {
                       ),
                     ),
                     onPressed: () {
-                      // Get.to(CheckOutPage());
-                      // Navigator.push(
-                      //     context,
-                      //     new MaterialPageRoute(
-                      //         builder: (context) => CheckOutPage()));
+                      Navigator.push(
+                          context,
+                          new MaterialPageRoute(
+                              builder: (context) => CheckOutPage()));
                     },
                   )),
               Padding(
@@ -85,51 +88,15 @@ class MyAppBar extends AppBar {
                 ),
               ),
             ]);
-
-  // ignore: non_constant_identifier_names
- static Widget Num_Of_Prod_in_Cart(){
-    // ignore: non_constant_identifier_names
-    var NumOfProd = 0;
-    return StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection("Users")
-            .doc(uid)
-            .collection("Cart")
-            .snapshots(),
-        builder: (context, snapshot) {
-          if(snapshot.hasData){
-            NumOfProd = snapshot.data!.docs.length;
-            return Text(NumOfProd.toString());
-          }
-          else{
-            return Text(NumOfProd.toString());
-          }
-        }
-    );
-
-  }
-
-  // ignore: non_constant_identifier_names
- static Widget Num_Of_Prod_in_Wishlist(){
-    // ignore: non_constant_identifier_names
-    var NumOfProd = 0;
-    return StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection("Users")
-            .doc(uid)
-            .collection("Wishlist")
-            .snapshots(),
-        builder: (context, snapshot) {
-          if(snapshot.hasData){
-            NumOfProd = snapshot.data!.docs.length;
-            return Text(NumOfProd.toString());
-          }
-          else{
-            return Text(NumOfProd.toString());
-          }
-        }
-    );
-
-  }
-
 }
+
+// class app {
+//   final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+//   Future<dynamic> navigateTo(String routeName) {
+//     return navigatorKey.currentState!.pushNamed(routeName);
+//   }
+
+//   bool goBack() {
+//     return navigatorKey.currentState.pop();
+//   }
+// }
