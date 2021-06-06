8851c0d0cf6a9429627a38478301dcd2ac78262e
Mmasehume <2089198@students.wits.ac.za>
2021-06-06 13:13:47 +0200

    google round button test

---
+++ test/components/google_round_button_test.dart
@@ -0,0 +1,16 @@
+import 'package:aishop/components/google_round_button.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  Widget makeTestableWidget({required Widget child}) {
+    return MaterialApp(
+      home: child,
+    );
+  }
+
+  testWidgets('google round button ...', (tester) async {
+    GoogleRoundButton googleRoundButton = GoogleRoundButton();
+    await tester.pumpWidget(makeTestableWidget(child: googleRoundButton));
+  });
+}
