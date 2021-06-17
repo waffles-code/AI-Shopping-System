02039b7dc6edf032d328a39e91bc9f1e4a2f4c0c
Mmasehume <2089198@students.wits.ac.za>
2021-06-17 13:34:14 +0200

    update test

--- test/widgets/category_test.dart
+++ test/widgets/category_test.dart
@@ -1,16 +1,38 @@
+import 'package:aishop/icons/icons.dart';
+import 'package:aishop/screens/Categories/books.dart';
+import 'package:aishop/screens/homepage.dart';
 import 'package:aishop/widgets/category.dart';
+import 'package:aishop/widgets/category_card.dart';
+import 'package:aishop/widgets/kitchen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  Widget testWidget = new MediaQuery(
+      data: new MediaQueryData(), child: new MaterialApp(home: new HomePage()));
+
   Widget makeTestableWidget({required Widget child}) {
     return MaterialApp(
       home: child,
     );
   }
 
-  testWidgets('category ...', (tester) async {
+  testWidgets('category cards visible ...', (tester) async {
     Category cat = Category();
     await tester.pumpWidget(makeTestableWidget(child: cat));
+
+    expect(find.byType(InkWell), findsWidgets);
+    expect(find.byType(CategoryCard), findsWidgets);
+    final books = find.widgetWithText(CategoryCard, "Books");
+    final shoes = find.widgetWithText(CategoryCard, "Shoes");
+    final tech = find.widgetWithText(CategoryCard, "Tech");
+    final kitchen = find.widgetWithText(CategoryCard, "Kitchen");
+    final clothes = find.widgetWithText(CategoryCard, "clothes");
+
+    await tester.tap(books);
+    await tester.tap(shoes);
+    await tester.tap(tech);
+    await tester.tap(kitchen);
+    await tester.tap(clothes);
   });
 }
